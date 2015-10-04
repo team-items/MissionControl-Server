8bf4afce50ee8bcf776d3e89d949f8f8514af837
Daniel Maximilian Swoboda <snoato@gmail.com>
2015-10-04 14:35:21 +0200

    Main Class now reads port number from config file and has debug output

--- main.py
+++ main.py
@@ -1,15 +1,39 @@
+#First Party Libraries
 import json
 
+#Own Libraries
+from mclib import *
+
+#Third Party Libraries
+from termcolor import colored
 
 class MissionControl():
 	CONFIGPATH = "config.conf";
+	PORT = 62626;
 
 	#Setup functions 
+	def setUp(self, configs):
+		settings = json.loads(configs);
 
+		if("Server" in settings.keys()):
+			if("Port" in settings["Server"].keys()):
+				self.PORT = settings["Server"]["Port"];
+			else:
+				printWarning("Missing Port in Config File, using 62626");
+		else:
+			printWarning("Invalid Configfile. Using default values only.");
+		
+		
 
 	#Constructor
 	def __init__(self):
-		configfile = open(self.CONFIGPATH, 'r');
+		try:
+			configfile = open(self.CONFIGPATH, 'r');
+			self.setUp(configfile.read());
+		except:
+			printError("No config file found.");
+			printWarning("proceeding with default values");
+
 
 	#Running Functions
 
