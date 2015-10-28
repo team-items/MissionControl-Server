0950604d683bda1c3bd72dc1e660cdb089864bee
Daniel Maximilian Swoboda <snoato@gmail.com>
2015-10-28 22:13:45 +0100

    Bugfixes and improvements on ClientManger
    *several additions and improvements on handshake handling function
    *correct if in handleHandshake function
    *improved output
    *server now sends correct MIDaC Data objects after handshake
    accomplishment

--- ClientManager.py
+++ ClientManager.py
@@ -10,6 +10,7 @@ class ClientManager():
 	inputready = None;
 	outputready = None;
 	server = None;
+	newConnectedId = 0;
 
 	log = None;
 
@@ -48,15 +49,20 @@ class ClientManager():
 		client, address = self.server.accept();
 		client.setblocking(0);
 
-		clients.append(Client(client, 2048));
+		self.clients.append(Client(client, 2048, address[0], address[1], self.newConnectedId));
 
-		self.log.logAndPrintMessage("New client ("+address+") connected")
+		self.log.logAndPrintMessage("Client "+`self.newConnectedId`+" ("+address[0]+":"+`address[1]`+") connected");
+		self.newConnectedId+=1;
 
 	def handleHandshake(self):
 		for client in self.getHandshakeSockets():
-			if client.socket in self.inputready and client.socket in self.outputready:
+			if (client.socket in self.inputready and (client.handshakeStatus == 0 or client.handshakeStatus == 3 )) or (client.socket in self.outputready and (client.handshakeStatus == 1 or client.handshakeStatus == 2)):
 				client.performHandshake();
+				if client.established:
+					self.log.logAndPrintSuccess("Handshake with Client "+`client.connectingId`+" successful!");
+			if client.socket in self.inputready:
 				self.inputready.remove(client.socket);
+			if client.socket in self.outputready:
 				self.outputready.remove(client.socket);
 
 	def handleInput(self):
@@ -64,18 +70,23 @@ class ClientManager():
 			if sock == self.server:
 				self.handleConnect();
 			else:
-				data = sock.recv(2048);
-				if data:
-					print(data.decode("utf-8"));
-				else:
-					self.inputready.remove(sock);
-					self.outputready.remove(sock);
-					self.clients.remove(self.getClientBySocket(sock));
+				try:
+					data = sock.recv(2048);
+					if data:
+						print(data.decode("utf-8"));
+					else:
+						self.inputready.remove(sock);
+						self.outputready.remove(sock);
+						client = self.getClientBySocket(sock);
+						self.log.logAndPrintWarning("Client "+`client.connectingId`+" ("+client.address+":"+`client.port`+") disconnected!");
+						self.clients.remove(client);
+				except socket.error:
+					self.log.logAndPrintWarning("Socket Error");
 
 
 	def handleOutput(self):
 		for sock in self.outputready:
-			sock.send("asdf");
+			sock.send("{'Data':{'Analog1':500, 'Analog2':510, 'Digital1': 'true', 'Digital2': 'false'}}");
 
 
 
