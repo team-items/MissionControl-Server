b51dd51049c36b6aa51f639b0f2a56c66bd14ccb
Daniel Maximilian Swoboda <snoato@gmail.com>
2015-11-20 18:10:16 +0100

    *added networkutil
    *added multi receive function to handle inbound messages that are
    possibly multiparted

---
+++ NetworkUtil.py
@@ -0,0 +1,23 @@
+import json
+import select;
+import socket;
+
+def multiReceive(client, SEGMENT_SIZE):
+	finished = False;
+	jsonMsg = None;
+	msg = client.recv(SEGMENT_SIZE).decode("utf-8");
+	if not msg:
+		return False;
+	while not finished:
+		try:
+			jsonMsg = json.loads(msg);
+
+			finished = True;
+		except ValueError:
+			inputready, outputready, excepts = select.select([client], [], []);
+			if len(inputready) == 1:
+				msg = msg+client.recv(SEGMENT_SIZE).decode("utf-8");
+
+				if not msg:
+					return False;
+	return jsonMsg;
