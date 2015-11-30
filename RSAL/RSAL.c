5076bab8ad66e077d5afebd239af387b059b701c
Daniel Maximilian Swoboda <snoato@gmail.com>
2015-11-30 20:47:23 +0100

    *rewrote RSAL

--- RSAL/RSAL.c
+++ RSAL/RSAL.c
@@ -2,14 +2,7 @@
 MIDaC KIPR Link ROSL (Robotic System Abstraction Layer)
 Version 0.1
 */
-
-#define true 1
-#define false 0
-
 #include <stdio.h>
-#include <unistd.h>
-#include <string.h> /* for strncpy */
-
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
@@ -19,118 +12,83 @@ Version 0.1
 #include "LINKV1.c"
 
 #define SOCK_PATH "echo_socket"
-#define MSG_LENGTH 2048
 
-void splitAndSend(char* msg);
-void sendMessage(char* message);
-char* recvMessage();
-
-int s, t, len, i, leng;
+int sock, t, len, i;
 struct sockaddr_un remote;
-char str[MSG_LENGTH],splitbuff[MSG_LENGTH], buff[2049];
+char str[2048];
 
-
-char *StringPadRight(char *string, int padded_len, char *pad) {
-    leng = (int) strlen(string);
-    if (leng >= padded_len) {
-        return string;
+void sendMsg(char* msg){
+    if(strlen(msg) > 2047){
+        int ward = (strlen(msg)/2048) + 1;
+        char* message = malloc(2048*ward);
+        strcpy(message, msg);
+        for(i=strlen(msg); i<2048*ward; i++){
+            message[i] = ' ';
+        }
+        message[2048*ward] = '\0';
+        if (send(sock, message, strlen(message), 0) == -1) {
+            perror("send");
+        }
+        free(message);
+    }else{
+        char* message = malloc(2048);
+        strcpy(message, msg);
+        for(i=strlen(msg); i<2048; i++){
+            message[i] = ' ';
+        }
+        message[2048] = '\0';
+        if (send(sock, message, strlen(message), 0) == -1) {
+            perror("send");
+        }
+        free(message);
     }
-    int i;
-    for (i = 0; i < padded_len - leng; i++) {
-        strcat(string, pad);
-    }
-    return string;
 }
 
-void splitAndSend(char* msg){
-    for(i=0; i<strlen(msg); i+=2047){
-        memcpy( splitbuff, &msg[i], 2047 );
-        splitbuff[2047] = '\0';
-        sendMessage(splitbuff);
-    }
-}
-
-
-void sendMessage(char* message){
-    if(strlen(message) <= 2048){
-        memcpy( buff, &message[0], 2048 );
-        message = StringPadRight(buff, 2048, " ");
-        message[2048] = '\0';      
+char* receiveMsg(){
+    if ((t=recv(sock, str, 2048, 0)) > 0) {
+        str[t] = '\0';
+        puts(str);
     } else {
-        splitAndSend(message);
+        if (t < 0) perror("recv");
+        else printf("Server closed connection\n");
+        exit(1);
     }
-
-    if (send(s, buff, strlen(buff), 0) == -1) {
-        //printf("RSAL_PROC_MESSAGE: Sending failed\n");  
-    }  else {
-        //printf("sendsuccess\n");
-    }
-    //msleep(50);
-}
-
-char* recvMessage(){
-    if( recv(s, str, MSG_LENGTH, 0) < 0)
-    {
-        printf("RSAL_PROC_MESSAGE: Receiving failed\n");
-        return "Failed";
-    } 
-
-    return str;
 }
 
 int main(void)
 {
 
-    if ((s = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+    if ((sock = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
         perror("socket");
         exit(1);
     }
 
-    printf("RSAL_PROC_MESSAGE: Trying to connect...\n");
+    printf("Trying to connect...\n");
 
     remote.sun_family = AF_UNIX;
     strcpy(remote.sun_path, SOCK_PATH);
-    len = strlen(remote.sun_path) + sizeof(remote.sun_family) +1;
-    if (connect(s, (struct sockaddr *)&remote, len) == -1) {
-        //perror("connect");
+    len = strlen(remote.sun_path) + sizeof(remote.sun_family) + 1;
+    if (connect(sock, (struct sockaddr *)&remote, len) == -1) {
+        perror("connect");
         exit(1);
     }
 
-    int status = 0;
-    while(status < 3){
-        if(status == 0){
-            sendMessage("{ \"B-Connect\" : \"\" }");
-            status = 1;
-            printf("RSAL_PROC_MESSAGE: Sent B-Connect Message\n");
-        } else if (status == 1){
-            recvMessage();
-            status = 2;
-            printf("RSAL_PROC_MESSAGE: Received Conn-Ack\n");
-        } else if (status == 2){
-           /*Send the connLao*/
-            sendMessage(generateConnLAO());
-            status = 3;
-            printf("RSAL_PROC_MESSAGE: Sent ConnLAO Message\n");
-        }   
-    }
-    printf("RSAL_PROC_MESSAGE: Connected\n");
+    printf("Connected.\n");
+    puts("Sending B-Connect");
+    sendMsg("{ \"B-Connect\" : \"\"}");
+    puts("Receiving ConnACK");
+    receiveMsg();
+    puts("Sending ConnLAO");
+    sendMsg(generateConnLAO());
 
-    
-    for(;;){
-
+    puts("Handshaking performed");
+    while(1) {
         char* msg = generateDataMsg();
-        sendMessage(msg);
+        sendMsg(msg);
+        free(msg);
     }
-        //if ((t=recv(s, str, MSG_LENGTH, 0)) > 0) {
-        //    str[t] = '\0';
-        //    printf("echo> %s", str);
-        //} else {
-        //    if (t < 0) perror("recv");
-        //    else printf("Server closed connection\n");
-        //    exit(1);
-        //}
 
-    close(s);
+    close(sock);
 
     return 0;
 }
