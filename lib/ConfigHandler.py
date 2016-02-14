a0e3f7797f1275d998085ba73d48342144bc7ca8
Daniel Maximilian Swoboda <snoato@gmail.com>
2016-02-14 20:25:40 +0100

    Updated host config
    *MissionControl Server now available on every interface of the Server
    device
    *Sometimes you should read the manual (with empty string as host
    address every interface is selected)

--- lib/ConfigHandler.py
+++ lib/ConfigHandler.py
@@ -3,50 +3,18 @@ import json
 import os
 from sys import platform as _platform
 
-def run_ifconifg(interface):
-	if _platform == "darwin":
-		return os.popen('ifconfig '+interface+' | grep "inet " | cut -d" " -f2 | cut -d" " -f1').read().strip()
-	else:
-		return os.popen('ifconfig '+interface+' | grep "inet\ addr" | cut -d: -f2 | cut -d" " -f1').read().strip()
 
-def is_ip(input):
-	inpArr = input.split(".")
-	if len(inpArr) != 4:
-		return False
-	for number in inpArr:
-		try:
-			int(number)
-		except ValueError:
-			return False
-
-	return True
 
 #Used to store, load and manage the configuration
 class ConfigHandler():
 	CONFIGPATH = "config.conf" 
 	PORT = 62626 
 	SAMPLERATE = 0.01 
-	HOST = 'localhost' 
+	HOST = '' 
 	BACKLOG = 20 
 	SEGMENT_SIZE = 2048 
 
 	log = None 
-
-
-	#gets ip via the unixoid ifconfig command
-	def get_host(self):
-		interfaces = ['en0', 'eth0', 'wlan0']
-
-		if _platform == "win32" or _platform == "win64":
-			self.log.logAndPrintWarning("Platform not supported, using 'localhost' for host") 
-		else:
-			for intf in interfaces:
-				ip = run_ifconifg(intf)
-				if is_ip(ip):
-					self.HOST = ip
-					return 
-
-	
 
 	#loads the config file and prints the running config
 	def __init__(self, path, log):
@@ -79,14 +47,11 @@ class ConfigHandler():
 				self.SEGMENT_SIZE = config["Segment_Size"] 
 			else:
 				self.log.logAndPrintWarning("No segment size in configfile, using default") 
-
-			self.get_host() 
 		else:
 			self.log.logAndPrintError("Invalid config file, using default values") 
 
 		ph = "                            " #placeholder string for better looking output
 
 		self.log.logAndPrintMessage("Running config: \n"+ph+"CONFIGPATH "+self.CONFIGPATH+
-			"\n"+ph+"PORT "+`self.PORT`+"\n"+ph+"SAMPLERATE "+`self.SAMPLERATE`+"\n"+ph+"HOST "+self.HOST+
-			"\n"+ph+"BACKLOG "+`self.BACKLOG`+"\n"+ph+"SEGMENT_SIZE "+`self.SEGMENT_SIZE`)
+			"\n"+ph+"PORT "+`self.PORT`+"\n"+ph+"SAMPLERATE "+`self.SAMPLERATE`+"\n"+ph+"BACKLOG "+`self.BACKLOG`+"\n"+ph+"SEGMENT_SIZE "+`self.SEGMENT_SIZE`)
 
